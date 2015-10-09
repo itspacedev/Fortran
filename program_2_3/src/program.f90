7fceb51ed43a72cc2492d6b8810b7d068ba29779
itspacedev <itspacedev@gmail.com>
2015-10-10 02:52:13 +0300

    Bug Fixes in program_2_3

--- program_2_3/src/program.f90
+++ program_2_3/src/program.f90
@@ -1,17 +1,15 @@
 program program_2_3
     implicit none
-    integer, parameter      :: R_ = 8
+    integer, parameter      :: R_ = 4
     character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt", E_ = "UTF-8"
     integer                 :: In=0, Out=0, N=0, C=0
     real(R_), allocatable   :: X(:)
-    !integer, allocatable    :: X(:)
     logical, allocatable    :: Pos(:)
 
     open (file=input_file, encoding=E_, newunit=In)
         read (In, *) N
         allocate  (X(N))
-        !read (In, '(f3.0)', advance='no') X
-        read (In, '(f5.8)') X
+        read (In, '(f4.3)') X
     close (In)
     
     !allocate (Pos(N), source=.false.)
