96cf77959373734b5dfa76dd94a4e6d3c6587981
itspacedev <itspacedev@gmail.com>
2015-10-17 00:10:49 +0300

    Program_7_17_v

--- program_7_17_v/src/program.f90
+++ program_7_17_v/src/program.f90
@@ -3,7 +3,7 @@ program program_6_1_v
     integer, parameter      :: R_ = 8
     character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt", E_ = "UTF-8"
     !character(:), allocatable :: form
-    integer                 :: In = 0, Out = 0, N = 0, i = 0, j = 0, Max = 0
+    integer                 :: In = 0, Out = 0, N = 0, i = 0, Max = 0, MaxE = 0, MinE = 0
     integer, allocatable    :: X(:, :)
     
     open (file=input_file, encoding=E_, newunit=In)
@@ -14,15 +14,24 @@ program program_6_1_v
     
     open (file=output_file, encoding=E_, newunit=Out)    
         write (Out, '(a, T4, "= ", i0)') "N", N
-        write (Out, '(5(i0, " "))') (X(i, :), i = 1, N)
+        write (Out, '(5(i4, " "))') (X(i, :), i = 1, N)
         
-        do i = 1, N
-            do j = 1, N
-                if (Abs(X(i, j)) > Max) then
-                    Max = Abs( X(i, j) )
-                end if
-            end do
-        end do
+        
+        MinE = Abs(MinVal(X))
+        MaxE = Abs(MaxVal(X))
+        if (MinE > MaxE) then
+            Max = MinE
+        else
+            Max = MaxE
+        end if
+
+        !do i = 1, N
+        !    do j = 1, N
+        !        if (Abs(X(i, j)) > Max) then
+        !            Max = Abs( X(i, j) )
+        !        end if
+        !    end do
+        !end do
         
         write (Out, *)
         write (Out, '(a, T7, "= ", i0)') "Max", Max
