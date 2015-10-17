26c766515fba063d21338c9b5a34d24e2fdcb5a9
itspacedev <itspacedev@gmail.com>
2015-10-17 21:23:50 +0300

    Updates

--- program_6_1_v/src/program.f90
+++ program_6_1_v/src/program.f90
@@ -3,7 +3,7 @@ program program_6_1_v
     integer, parameter      :: R_ = 8
     character(*), parameter :: input_file = "../data/input.txt", output_file = "output.txt", E_ = "UTF-8"
     integer                 :: In = 0, Out = 0, n = 0, f = 1
-    real(R_)                :: x = 0, ex = 1, item = 0, new_item = 0
+    real(R_)                :: x = 0, ex = 1, ex2 = 0, item = 0, new_item = 0
     
     open (file=input_file, encoding=E_, newunit=In)
         read (In, *) x    
@@ -14,39 +14,30 @@ program program_6_1_v
     item = x
     new_item = item / f 
     ex = ex + new_item
-    
-    open (file=output_file, encoding=E_, newunit=Out)    
-    
-    write (Out, *) "item_1= ", item
-    write (Out, *) "ex1= ", ex 
+    ex2 = ex
 
-    do
-        n = n + 1
-        
-        item = new_item
-        new_item = (item * x)
-        write (Out, *) "item*x= ", new_item
-        new_item = new_item / n
-        write (Out, *) "item/n= ", new_item
-        
-        !f = f * n
-        !new_item = new_item / f
-        
-        write (Out, *) "n= ", n
-        write (Out, *) "item= ", item
-        write (Out, *) "new_item= ", new_item 
-        !if (new_item == item) then
-        if (new_item == 0_R_) then
-            exit
-        else
-            ex = ex + new_item
-        end if
-        write (Out, *) "ex=", ex
-    end do     
+    open (file=output_file, encoding=E_, newunit=Out)
 
-    !open (file=output_file, encoding=E_, newunit=Out)
+        do
+            n = n + 1
+        
+            item = new_item
+            new_item = (item * x) / n
+            write (Out, '(a, T7, " ", f8.4)') "item", new_item
+        
+            ex2 = ex + new_item
+
+            if (ex2 == ex) then
+                exit
+            else
+                ex = ex2
+            end if
+
+            write (Out, '(a, T7, " ", f8.4)') "Exp", ex
+        end do 
+
         write (Out, *) "n=", n
-        write (Out, '(a, T7, "= ", f30.10)') "ex", ex
+        write (Out, '(a, T7, "= ", f8.4)') "Exp", ex
         write (Out, *) "error=", Exp(x) - ex
     close (Out)
 end program program_6_1_v
