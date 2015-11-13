332c4351371d59c6c29f9c435ffc479368c5fd19
itspacedev <itspacedev@gmail.com>
2015-11-14 00:10:25 +0300

    test

--- lab_1/src/environment.f90
+++ lab_1/src/environment.f90
@@ -1,33 +1,54 @@
-module Environment
-    use ISO_Fortran_Env
+module environment
+   use ISO_Fortran_Env
 
-    implicit none
-    integer, parameter      :: I_ = INT32
-    integer, parameter      :: R_ = REAL32
-    integer, parameter      :: C_ = R_
-    integer, parameter      :: CH_ = Selected_Char_Kind("ISO_10646")
-    character(*), parameter :: E_ = "UTF-8"
+   implicit none
+    
+   integer, parameter      :: I_ = INT32                             ! Разновидность типа для целочисленных переменных.
+   integer, parameter      :: R_ = REAL32                            ! Разновидность типа для вещественных переменных.
+   integer, parameter      :: C_ = R_                                ! Разновидность типа для компексных переменных.
+   integer, parameter      :: CH_= Selected_Char_Kind("ISO_10646")   ! Разновидность типа для символов.
+   character(*), parameter :: E_ = "UTF-8"                           ! Кодировка файлов.
 
-    interface operator (//)
-        module procedure Int_plus_string
-        module procedure String_plus_int
-    end interface
+   interface operator (//)
+      module procedure Int_plus_string
+      module procedure String_plus_int
+   end interface
 
 contains
 
-    pure function Int_plus_string(int, str) result(res)
-        integer, intent(in)         :: int
-        character(*), intent(in)    :: str
-        character(Len(str) + Max(Floor(Log10(Real(int, real64))) + 1, 1))   :: res
-        
-        write (res, '(i0, a)') int, str
+   pure function Int_plus_string(int, str) result(res)
+      integer, intent(in)                                         :: int
+      character(*), intent(in)                                    :: str
+      character(len(str)+Max(Floor(Log10(Real(int, I_*2)))+1, 1)) :: res
+
+      write (res, '(i0, a)') int, str
    end function Int_plus_string
 
    pure function String_plus_int(str, int) result(res)
-        integer, intent(in)         :: int
-        character(*), intent(in)    :: str
-        character(Len(str) + Max(Floor(Log10(Real(int, I_*2))) + 1, 1))   :: res
-        write (res, '(a, i0)') str, int
+      character(*), intent(in)                                    :: str
+      integer, intent(in)                                         :: int
+      character(len(str)+Max(Floor(Log10(Real(int, I_*2)))+1, 1)) :: res
+
+      write (res, '(a, i0)') str, int
    end function String_plus_int
 
-end module Environment
+   ! Обработка статуса ввода/вывода.
+   subroutine Handle_IO_status(IO, where)
+      integer, intent(in)        :: IO
+      character(*), intent(in)   :: where
+
+      integer Out
+
+      Out = OUTPUT_UNIT
+      open (Out, encoding=E_)
+      select case(IO)
+         case(0, IOSTAT_END)
+         case(1:)
+            write (Out, '(a, i0)') "Error " // where // ": ", IO
+         case default
+            write (Out, '(a, i0)') "Undetermined behaviour has been reached while " // where // ": ", IO
+      end select
+      ! close (Out) ! Если не OUTPUT_UNIT.
+   end subroutine Handle_IO_status
+
+end module environment
--- lab_1/src/program.f90
+++ lab_1/src/program.f90
@@ -1,36 +1,34 @@
 program lab_1
-    use Environment
+    use environment
     
     implicit none
 
-    integer, parameter              :: STUD_AMOUNT = 6, SURNAMES_LEN = 15, INITIALS_LEN = 5, YEARS_LEN = 4
-    character(kind=CH_), parameter  :: MALE = Char(Int(z'041C'), CH_)
-    character(:), allocatable       :: input_file, output_file, format, format2
+    integer, parameter                  :: STUD_AMOUNT = 8, SURNAMES_LEN = 15, INITIALS_LEN = 5, YEARS_LEN = 4
+    character(kind=CH_), parameter      :: MALE = Char(Int(z'041C'), CH_)
+    character(:), allocatable           :: input_file, output_file, format, format2
     
-    character(SURNAMES_LEN, kind=CH_) :: Surnames(STUD_AMOUNT) = CH__""
-    character(INITIALS_LEN, kind=CH_) :: Initials(STUD_AMOUNT) = CH__""
-
+    character(SURNAMES_LEN, kind=CH_)   :: Surnames(STUD_AMOUNT) = CH__""
+    character(INITIALS_LEN, kind=CH_)   :: Initials(STUD_AMOUNT) = CH__""
     character(SURNAMES_LEN, kind=CH_), allocatable :: Boys_Surnames(:)
-    character(INITIALS_LEN, kind=CH_), allocatable ::  Boys_Initials(:)
-    integer, allocatable              :: Boys_Years(:), Boys_Age(:), Postfix_Arr(:)
-    
-    character(kind=CH_)               :: Gender(STUD_AMOUNT) = CH__""!, postfix_end_char = CH__""
-    logical, allocatable              :: Is_A_Boy(:)
-    integer                           :: Years(STUD_AMOUNT) = 0
-    integer :: In, Out, IO, i, Boys_Amount = 0, Boys_Avg = 0
-    integer :: CYEAR = 2015
-    integer, parameter :: INT_C = 5 
-    character(:), allocatable :: postfix
-    !character(10, kind=CH_) :: postfix
+    character(INITIALS_LEN, kind=CH_), allocatable :: Boys_Initials(:)
+
+    integer, allocatable                :: Boys_Years(:), Boys_Age(:)
+    character(kind=CH_)                 :: Gender(STUD_AMOUNT) = CH__""
+    logical, allocatable                :: Is_A_Boy(:)
+    integer                             :: Years(STUD_AMOUNT) = 0
+    integer                             :: In, Out, IO, i, Boys_Amount = 0, Boys_Avg = 0, CYEAR = 2015
+    character(:), allocatable           :: postfix
 
     input_file  = "../data/input.txt"
     output_file = "output.txt"
     
+    ! Read file
     open (file=input_file, encoding=E_, newunit=In)
         format = '(3(a, 1x), i4)'
         read (In, format, iostat=IO) (Surnames(i), Initials(i), Gender(i), Years(i), i = 1, STUD_AMOUNT)
     close (In)
 
+    ! Handle read error    
     Out = OUTPUT_UNIT
     open (Out, encoding=E_)
         select case (io)
@@ -42,14 +40,28 @@ program lab_1
             case default
                 write (Out, *) "Unknown error: ", io
         end select
-
+    
+    ! Write initial list
     open (file=output_file, encoding=E_, newunit=Out)
-        !write (Out, *) "Source: " // 12
         write (Out, '(a)') "Исходный список:"
         write (Out, format, iostat=IO) (Surnames(i), Initials(i), Gender(i), Years(i), i = 1, STUD_AMOUNT)
     close (Out)
+    
+    ! Handle write error
+    Out = OUTPUT_UNIT
+    open (Out, encoding=E_)
+        select case (io)
+            case(0)
+            case(IOSTAT_END)
+                write (Out, *) "End of file has been reached while writing class"
+            case(1:)
+                write (Out, *) "Error while writing class list: ", io
+            case default
+                write (Out, *) "Unknown error: ", io
+        end select
 
-    Is_A_Boy      = Gender == MALE ! Char(Int(z'041C', CH_))
+    ! Main Logic
+    Is_A_Boy      = Gender == MALE
     Boys_Amount   = Count(Is_A_Boy)
 
     Boys_Surnames = Pack(Surnames, Is_A_Boy)
@@ -59,9 +71,8 @@ program lab_1
     Boys_Age = CYEAR - Boys_Years
     Boys_Avg = Ceiling(Real(Sum(Boys_Age) / Boys_Amount, R_))
 
+    ! Write result
     open (file=output_file, encoding=E_, newunit=Out, position='append')
-        write(Out, *) (Postfix_Arr(i), i =1,Size(Postfix_Arr))
-        write(Out, *) 'lol' // INT_C        
         write(Out, '(/a)') "Список юношей:"
         format2 = '(3(a, 1x), i4, 1x,  i0, " лет")'
 
@@ -70,13 +81,26 @@ program lab_1
 
         select case (Mod(Boys_Avg, 10))
             case (1)
-                postfix = 'god'
+                postfix = 'год'
             case (2:4)
-                postfix = 'goda'
+                postfix = 'года'
             case default
-                postfix = 'let'
+                postfix = 'лет'
         endselect
-        
         write(Out, '(/a, i0, 1x, a)') "Средний возраст юношей: ", Boys_Avg, postfix
-   close (Out) 
+   close (Out)
+
+    ! Handle write error
+    Out = OUTPUT_UNIT
+    open (Out, encoding=E_)
+        select case (io)
+            case(0)
+            case(IOSTAT_END)
+                write (Out, *) "End of file has been reached while writing class"
+            case(1:)
+                write (Out, *) "Error while writing class list: ", io
+            case default
+                write (Out, *) "Unknown error: ", io
+        end select
+
 end program lab_1
